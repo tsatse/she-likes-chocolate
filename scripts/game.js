2d3c020351755edefaba19efa059d971999d858b
tsatse <joachim.revol-tissot.ext@francetv.fr>
2015-07-09 12:53:10 +0200

    linting

--- scripts/game.js
+++ scripts/game.js
@@ -23,12 +23,14 @@
                     result[imageName] = null;
                 }
             }
-        };
+        }
+
         window.onload = function() {
             function game_loop() {
                 update();
                 draw();
             }
+            
             function update() {
                 var character_list = Object.keys(characters);
                 for(var i = 0 ; i < character_list.length ; i++) {
@@ -41,17 +43,19 @@
                     }
                 }
             }
+            
             function draw_character(character_name) {
                 var character = characters[character_name];
                 var image = images[character.sprites[character.action]];
-                var current_map_offset = get_map_offset(characters['me'].x, characters['me'].y);
+                var current_map_offset = get_map_offset(characters.me.x, characters.me.y);
                 ctx.drawImage(image,
                     0, 0, image.width, image.height,
                     character.x - current_map_offset.x, character.y - current_map_offset.y,
-                    image.width, image.height)
+                    image.width, image.height);
             }
+
             function get_map_offset(x, y) {
-                var result = {x:0,y:0}
+                var result = {x:0,y:0};
                 if(x > (game_canvas.width/2)) {
                     result.x = Math.min(x - game_canvas.width/2, map_width - game_canvas.width);
                 }
@@ -65,15 +69,15 @@
             }
             function draw_map(x, y) {
                 var map_offset = get_map_offset(x, y);
-                ctx.drawImage(images['sky'], 0, 0, game_canvas.width, game_canvas.height, 0, 0, game_canvas.width, game_canvas.height);
-                ctx.drawImage(images['houses'],
+                ctx.drawImage(images.sky, 0, 0, game_canvas.width, game_canvas.height, 0, 0, game_canvas.width, game_canvas.height);
+                ctx.drawImage(images.houses,
                     Math.min(map_offset.x, map_width-game_canvas.width), map_offset.y, game_canvas.width, game_canvas.height,
                     0, 0, game_canvas.width, game_canvas.height);
                 
             }
             function draw_foreground(x, y) {
                 var map_offset = get_map_offset(x, y);
-                ctx.drawImage(images['foreground'],
+                ctx.drawImage(images.foreground,
                     (map_offset.x*1.5)%(map_width-game_canvas.width), map_offset.y, game_canvas.width, game_canvas.height,
                     0, 0, game_canvas.width, game_canvas.height);
             }
@@ -86,7 +90,7 @@
                 return false;
             }
             function draw(time, bounding_element) {
-                draw_map(characters['me'].x, characters['me'].y);
+                draw_map(characters.me.x, characters.me.y);
                 var character_list = Object.keys(characters);
                 character_list.sort(function(a, b) {
                     return characters[a].y - characters[b].y;
@@ -96,43 +100,43 @@
                         draw_character(character_list[i]);
                     }
                 }
-                draw_foreground(characters['me'].x, characters['me'].y);
+                draw_foreground(characters.me.x, characters.me.y);
             }
             document.addEventListener('keydown', function(event) {
                     var unit = 2;
-                    characters['me'].action = 'idle';
+                    characters.me.action = 'idle';
                     if(event.keyCode == 37) {
-                        characters['me'].dx = -unit;
+                        characters.me.dx = -unit;
                     }
                     if(event.keyCode == 39) {
-                        characters['me'].dx = unit;
+                        characters.me.dx = unit;
                     }
                     if(event.keyCode == 38) {
-                        characters['me'].dy = -unit;
+                        characters.me.dy = -unit;
                     }
                     if(event.keyCode == 40) {
-                        characters['me'].dy = unit;
+                        characters.me.dy = unit;
                     }
                     
-                    if(characters['me'].dx > 0) {
-                        characters['me'].action = 'walk_right';
+                    if(characters.me.dx > 0) {
+                        characters.me.action = 'walk_right';
                     }
-                    if(characters['me'].dx < 0) {
-                        characters['me'].action = 'walk_left';
+                    if(characters.me.dx < 0) {
+                        characters.me.action = 'walk_left';
                     }
                 }, false);
             document.addEventListener('keyup', function(event) {
                     if(event.keyCode == 37) {
-                        characters['me'].dx = 0;
+                        characters.me.dx = 0;
                     }
                     if(event.keyCode == 39) {
-                        characters['me'].dx = 0;
+                        characters.me.dx = 0;
                     }
                     if(event.keyCode == 38) {
-                        characters['me'].dy = 0;
+                        characters.me.dy = 0;
                     }
                     if(event.keyCode == 40) {
-                        characters['me'].dy = 0;
+                        characters.me.dy = 0;
                     }
                 }, false);
 
@@ -188,12 +192,12 @@
                 },
                 function(imgs) {
                     images = imgs;
-                    map_width = images['houses'].width;
+                    map_width = images.houses.width;
                     game_canvas.width = 800;
                     game_canvas.height = 450;
                     Framer.reset();
                     Framer.start();
                     Framer.add(game_loop);
                 });
-        }
+        };
 });
