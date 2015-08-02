6c2570f11a1e5fffbe137c5e000c5cb37044c05c
tsatse <joachim.revol-tissot.ext@francetv.fr>
2015-08-03 01:04:38 +0200

    fixed titles reacting too fast to key presses

--- src/gameplay/press-any-key.js
+++ src/gameplay/press-any-key.js
@@ -2,7 +2,23 @@ function PressAnyKey() {
 }
 
 PressAnyKey.prototype = {
+    init: function init() {
+        this.armed = false;
+    },
+    
     update: function(time) {
+        if(!this.armed) {
+            if(!Object.keys(this.host.keys)
+                .map(function(key) {
+                    return this.host.keys[key];
+                }.bind(this))
+                .some(function(key) {
+                    return key;
+                })) {
+                this.armed = true;
+            }
+            return;
+        }
         if(Object.keys(this.host.keys)
             .map(function(key) {
                 return this.host.keys[key];
