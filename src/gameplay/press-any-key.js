205b354ff7e520d9efec5f0ee4ce09ebd6bfddfa
tsatse <joachim.revol-tissot.ext@francetv.fr>
2015-08-03 13:19:39 +0200

    added other ways to end a title phase

--- src/gameplay/press-any-key.js
+++ src/gameplay/press-any-key.js
@@ -4,8 +4,27 @@ function PressAnyKey() {
 PressAnyKey.prototype = {
     init: function init() {
         this.armed = false;
+        var timeout = this.host.phaseInstances[this.host.phaseName].timeout;
+        if(timeout) {
+            this.timer = setTimeout(function() {
+                this.host.gotoSink('keyPressed');
+            }.bind(this), timeout);
+        }
+        this._onClick = function() {
+            this._cleanUpAndGo();
+        }.bind(this);
+        document.body.addEventListener('click', this._onClick);
     },
-    
+
+    _cleanUpAndGo: function _cleanUp() {
+        if(this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
+        document.body.removeEventListener('click', this._onClick);
+        this.host.gotoSink('keyPressed');
+    },
+
     update: function(time) {
         if(!this.armed) {
             if(!Object.keys(this.host.keys)
@@ -26,7 +45,7 @@ PressAnyKey.prototype = {
             .some(function(key) {
                 return key;
             })) {
-            this.host.gotoSink('keyPressed');
+            this._cleanUpAndGo();
         }
         return true;
     }
